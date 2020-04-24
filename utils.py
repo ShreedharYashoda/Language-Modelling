39ff7bf12b8ae8ec770953ba869037dbdd14aa30
ShreedharYashoda <shreedhar022@gmail.com>
2020-04-24 12:02:04 -0500

    Utils edit

--- utils.py
+++ utils.py
@@ -132,5 +132,29 @@ def save_json(dictionary, filename):
     with open(filename, 'w') as fp:
         json.dump(dictionary, fp)
 
+class BatchGenerator(object):
+
+    def __init__(self, data, num_steps, batch_size, total_words, skip_step=5):
+        self.data = data
+        self.num_steps = num_steps
+        self.batch_size = batch_size
+        self.total_words = total_words
+        self.current_idx = 0
+        self.skip_step = skip_step
+
+    def generate(self):
+        x = np.zeros((self.batch_size, self.num_steps))
+        y = np.zeros((self.batch_size, self.num_steps, self.total_words))
+        while True:
+            for i in range(self.batch_size):
+                if self.current_idx + self.num_steps >= len(self.data):
+                    self.current_idx = 0
+                x[i, :] = self.data[self.current_idx:self.current_idx + self.num_steps]
+                temp_y = self.data[self.current_idx +
+                                   1:self.current_idx + self.num_steps + 1]
+                y[i, :, :] = tf.keras.utils.to_categorical(
+                    temp_y, num_classes=self.total_words)
+                self.current_idx += self.skip_step
+            yield x, y
 
 
